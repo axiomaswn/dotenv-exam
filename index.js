49e38fcc8552ba110895b86ddc4dccc0738e9558
axiomaswn <xyz@this>
2017-06-19 11:46:30 +0700

    different dir

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require("dotenv").config()
+require("dotenv").config({path: './config/.env'})
+// require("dotenv").config() //same directory
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
